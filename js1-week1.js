977d9ce7c804c7ab6aad70972c0475e072228a3f
SaharNaderi <108987674+SaharNaderi@users.noreply.github.com>
2022-11-19 16:35:23 +0000

    week1jslearn

---
+++ js1-week1.js
@@ -0,0 +1,76 @@
+/*console.log("hello world")
+var first= 1;
+console.log(first)
+
+let x = "John";
+console.log(x)
+
+let y= 'sam'
+console.log(y)
+
+let z=`nad`
+console.log(z)*/
+
+/*let function=`nad`
+console.log(typeof z)*/
+
+/*let secondword = "profeshnal";
+let combineString = `Sahar${secondword}designer`;
+console.log(combineString);*/
+
+/*let secondword = "profeshnal";
+let combineString = "Sahar${secondword}designer";
+console.log(combineString);*/
+
+
+/*let numberOfTrainees = 47;
+let numberOfMentors = 9;
+let totalpeople= numberOfTrainees+numberOfMentors;
+console.log(totalpeople);
+
+let percentageTreniees=numberOfTrainees/totalpeople*100;
+console.log(Math.round(percentageTreniees));
+
+let percentageMentors=numberOfMentors/totalpeople*100;
+console.log(percentageMentors);
+console.log(1+1);
+const x="red"
+;*/
+
+
+function formatPenceToPound(num){
+   
+    let pence=num;
+    let pound=Number(pence/100);
+   
+   
+    return "£"+pound
+
+}
+console.log(formatPenceToPound(10)); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
